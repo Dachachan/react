0ab6af26cd09c5489d68298c2b8ab351f6eabb66
Dachachan <614259036@webmail.npru.ac.th>
2021-03-16 21:17:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
+import ImgList from './Components/ImgList';
+import SearchForm from './Components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			imgs: [],
+			loadingState: true
+		};
+	}
+
+	componentDidMount() {
+		this.formSearch();
+	}
+
+	formSearch = (query = 'sea') => {
+		axios
+			.get(
+				`https://api.unsplash.com/search/photos/?page=1&per_page=12&query=${query}
+				&client_id=QsGD7I23FPbR1StTxWzjKj0JhDySkEPCUg6-AmkMTZc`
+			)
+			.then(data => {
+				this.setState({ imgs: data.data.results, loadingState: false });
+			})
+			.catch(err => {
+				console.log('Error happened during fetching!', err);
+			});
+	};
+
+	render() {
+		return (
+			<div>
+				<div className="main-header">
+					<div className="inner">
+						<h1 className="main-title">ImageSearch</h1>
+						<SearchForm onSearch={this.formSearch} />
+					</div>
+				</div>
+				<div className="main-content">
+					{this.state.loadingState
+						? <p>Loading</p>
+						: <ImgList  data={this.state.imgs} />}
+				</div>
+			</div>
+		);
+	}
 }
-
-export default App;
